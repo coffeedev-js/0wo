c71ceda79a22e2868b7650fd239e6f2ed2555b76
xtakoyakii <50556930+xtakoyakii@users.noreply.github.com>
2019-07-03 22:42:49 +0200

    Main files
    Uploaded main files

---
+++ Js.js
@@ -0,0 +1,30 @@
+// Js for Kaomoji Website
+
+/*Button to top*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+}
+
+/*Script for Side Nav*/
+		
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
